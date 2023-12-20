9ef5ab4054c49e46da7c5ea3f928b79314c9190d
Nathan Stader <nathan.stader@heig-vd.ch>
2023-12-20 15:46:12 +0100

    Modification de app.svelte en titre

--- src/app.svelte
+++ src/app.svelte
@@ -12,7 +12,7 @@
   <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container d-flex justify-content-between">
       <a href="/" class="navbar-brand d-flex align-items-center">
-        <strong>WOPR</strong>
+        <h1>WOPR</h1>
       </a>
     </div>
   </div>
